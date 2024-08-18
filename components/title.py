56981584b5c97f5e14bd247b753eff39914ce168
Robin Monjo <robinmonjo@gmail.com>
2024-08-18 13:47:03 +0200

    move css inline

--- components/title.py
+++ components/title.py
@@ -1,5 +1,32 @@
 from fasthtml.common import *
 
+style = """
+me {
+  background: linear-gradient(
+    to right,
+    #7953cd 20%,
+    #00affa 30%,
+    #0190cd 70%,
+    #764ada 80%
+  );
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  text-fill-color: transparent;
+  background-size: 500% auto;
+  animation: textShine 5s ease-in-out infinite alternate;
+  margin-bottom: 0;
+}
+
+@keyframes textShine {
+  0% {
+    background-position: 0% 50%;
+  }
+  100% {
+    background-position: 100% 50%;
+  }
+}
+"""
 
 def Title(text):
-    return H1(text, StyleX("components/gradient.css"))
+    return H1(text, Style(style))
