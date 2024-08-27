0301dd6a3493dfb2cb0cef0b3e001c28ad9b5c34
Robin Monjo <robinmonjo@gmail.com>
2024-08-27 10:04:46 +0200

    allow serving model using llama-cpp directly

--- Makefile
+++ Makefile
@@ -13,5 +13,8 @@ prod-run:
 remote-build: export DOCKER_HOST=ssh://root@[2a01:4f8:1c1b:b920::1]
 remote-build: build
 
-start-llm:
+serve-llm:
 	python -m llama_cpp.server --model=models/Phi-3.1-mini-4k-instruct-Q4_K_M.gguf --n_ctx=4096
+
+serve-llm2:
+	./llm/llama-server -m models/Phi-3.1-mini-4k-instruct-Q4_K_M.gguf --host 0.0.0.0 --port 8000 -cnv --mlock
--- llm/client.py
+++ llm/client.py
@@ -9,18 +9,13 @@ class Client:
             api_key="unused",
         )
         self.context = {"role": "system", "content": context()}
-        self.model_warmed_up = False
 
     def chat_completion(self, messages, stream=False):
         messages = [self.context] + messages
 
-        result = self.client.chat.completions.create(
+        return self.client.chat.completions.create(
             model="unused",
             messages=messages,
             stream=stream,
+            extra_body={"cache_prompt": True}
         )
-
-        if not stream:
-            self.model_warmed_up = True  # if stream it returns directly
-
-        return result
