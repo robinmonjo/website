da0361d47facd1109f20becf44084721652803cc
Robin Monjo <robinmonjo@gmail.com>
2024-08-28 12:39:03 +0200

    add tokenize fn

--- llm/client.py
+++ llm/client.py
@@ -1,11 +1,14 @@
 from openai import OpenAI
+import requests
 from llm.context import context
+
+SERVER_URL = "http://localhost:8000"
 
 
 class Client:
     def __init__(self):
         self.client = OpenAI(
-            base_url="http://localhost:8000/v1",
+            base_url=f"{SERVER_URL}/v1",
             api_key="unused",
         )
         self.context = {"role": "system", "content": context()}
@@ -19,3 +22,11 @@ class Client:
             stream=stream,
             extra_body={"cache_prompt": True},
         )
+
+    def tokenize(self, text):
+        data = {"content": text}
+        response = requests.post(f"{SERVER_URL}/tokenize", json=data)
+        return response.json()["tokens"]
+
+    def initial_prompt_size(self):
+        return len(self.tokenize(context()))
