4a82acba30924579d1e676edef27222cd6653147
Robin Monjo <robinmonjo@gmail.com>
2025-01-08 15:41:54 +0100

    bump a few other dependencies

--- nbs/sync_liked_tweets.py
+++ nbs/sync_liked_tweets.py
@@ -17,19 +17,17 @@
 from twikit import Client
 import json
 
+
 async def main():
 
     # In[2]:
 
-
     with open("raw_x_cookies.json", "r") as file:
         cookies = json.load(file)
-
 
     # Convert cookie to twikit format
 
     # In[3]:
-
 
     result = {}
     for item in cookies:
@@ -41,30 +39,22 @@ async def main():
     with open("x_cookies.json", "w") as file:
         json.dump(result, file, indent=4)
 
-
     # In[4]:
-
 
     client = Client("en-US")
     client.load_cookies("x_cookies.json")
 
-
     # In[5]:
-
 
     user = await client.get_user_by_screen_name("b0baille")
 
-
     # In[6]:
-
 
     # tweets = await user.get_tweets("Likes", 20)
     # for t in tweets:
     #     print(f"{t.media} - {t.media and len(t.media)}")
 
-
     # In[7]:
-
 
     import sqlite3
 
@@ -75,7 +65,8 @@ async def main():
     cursor = conn.cursor()
 
     # Create a table only if it doesn't already exist
-    cursor.execute("""
+    cursor.execute(
+        """
         CREATE TABLE IF NOT EXISTS tweets (
             id TEXT PRIMARY KEY,
             created_at DATETIME NOT NULL,
@@ -89,14 +80,13 @@ async def main():
             user_profile_image_url TEXT NOT NULL,
             user_screen_name TEXT NOT NULL
         )
-    """)
+    """
+    )
 
     # Commit the changes
     conn.commit()
 
-
     # In[8]:
-
 
     def import_tweet(tweet, count):
         # check if tweet already in database
@@ -107,29 +97,29 @@ async def main():
             print(f"Tweet {tweet.id} already in database")
             return False
 
-        cursor.execute("""
+        cursor.execute(
+            """
         INSERT INTO tweets (id, created_at, user_name, text, thumbnail_title, thumbnail_url, urls, full_text, media, user_profile_image_url, user_screen_name)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        """, (
-            tweet.id,
-            tweet.created_at_datetime,
-            tweet.user.name,
-            tweet.text,
-            tweet.thumbnail_title,
-            tweet.thumbnail_url,
-            json.dumps(tweet.urls),
-            tweet.full_text,
-            json.dumps(tweet.media),
-            tweet.user.profile_image_url,
-            tweet.user.screen_name
-            )
+        """,
+            (
+                tweet.id,
+                tweet.created_at_datetime,
+                tweet.user.name,
+                tweet.text,
+                tweet.thumbnail_title,
+                tweet.thumbnail_url,
+                json.dumps(tweet.urls),
+                tweet.full_text,
+                json.dumps(tweet.media),
+                tweet.user.profile_image_url,
+                tweet.user.screen_name,
+            ),
         )
 
         return True
 
-
     # In[9]:
-
 
     async def iterate_tweets(fn):
         tweets = await user.get_tweets("Likes", 100)
@@ -143,7 +133,8 @@ async def main():
 
         while True:
             tweets = await tweets.next()
-            if not tweets: return
+            if not tweets:
+                return
 
             for t in tweets:
                 if fn(t, count):
@@ -151,17 +142,13 @@ async def main():
                 else:
                     return
 
-
     # In[10]:
-
 
     def count_tweets():
         cursor.execute("SELECT COUNT(*) FROM tweets")
         return cursor.fetchone()[0]
 
-
     # In[11]:
-
 
     count_before = count_tweets()
     print(f"{count_before} entries before import")
@@ -174,43 +161,40 @@ async def main():
     print(f"{count_after} entries after import")
     print(f"{count_after - count_before} tweets imported")
 
-
     # In[12]:
-
 
     from datetime import datetime
 
-
     # In[13]:
 
-
     # Create a table only if it doesn't already exist
-    cursor.execute("""
+    cursor.execute(
+        """
         CREATE TABLE IF NOT EXISTS last_synchronized_at (
             id INTEGER PRIMARY KEY CHECK (id = 1),
             datetime TEXT
         )
-    """)
+    """
+    )
 
     ts = datetime.now()
-    cursor.execute("""
+    cursor.execute(
+        """
         INSERT OR REPLACE INTO last_synchronized_at (id, datetime) VALUES (1, ?)
-    """, (ts,))
+    """,
+        (ts,),
+    )
 
     # Commit the changes
     conn.commit()
 
-
     # In[14]:
-
 
     cursor.execute("SELECT datetime FROM last_synchronized_at WHERE id = 1")
     ts = cursor.fetchone()[0]
     print(f"Last updated at: {ts}")
 
-
     # In[15]:
-
 
     conn.close()
 
